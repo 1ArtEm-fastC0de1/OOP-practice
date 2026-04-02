07413dca4ed32e8db4f5410b5a7bd3779b5fb6b2
Artem Lenko <112342877+1ArtEm-fastC0de1@users.noreply.github.com>
2026-04-02 21:21:12 +0300

    Refactor task5 class and add TableView functionality

--- Main/src/task5.java
+++ Main/src/task5.java
@@ -1,250 +1,324 @@
 import java.io.*;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 /**
- * Приклад: обробка колекцій з використанням шаблонів Singleton, Command та макрокоманд
- * Демонструє:
- * - Singleton: Application
- * - Command: ChangeItemCommand, GenerateConsoleCommand, Save/Restore/View
- * - Макрокоманда: Menu
- * - Undo операції
- * @author xone
- * @version 1.0
+ * Головний клас task5
  */
 public class task5 {
 
-    // ===================== Item2d =====================
-    public static class Item2d implements Serializable {
-        private static final long serialVersionUID = 1L;
-        private double x, y;
-
-        public Item2d() { this(0,0); }
-        public Item2d(double x, double y) { this.x=x; this.y=y; }
-
-        public void setXY(double x, double y) { this.x=x; this.y=y; }
-        public double getX() { return x; }
-        public double getY() { return y; }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (!(obj instanceof Item2d)) return false;
-            Item2d o = (Item2d)obj;
-            return Double.compare(x, o.x)==0 && Double.compare(y, o.y)==0;
+    public static void main(String[] args) {
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        @Override
-        public String toString() { return String.format("(%.0f; %.3f)", x, y); }
+        task5 t = new task5();
+        t.run();
     }
 
-    // ===================== Інтерфейси Command =====================
-    public interface Command { void execute(); }
-    public interface ConsoleCommand extends Command { char getKey(); }
+    public void run() {
+        TableView view = new TableView();
 
-    // ===================== ChangeItemCommand =====================
-    public static class ChangeItemCommand implements Command {
-        private Item2d item;
-        private double offset;
-        private double prevY; // Для undo
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введіть ширину таблиці: ");
+        view.setWidth(sc.nextInt());
+        System.out.print("Введіть кількість чисел: ");
+        view.setCount(sc.nextInt());
 
-        public Item2d setItem(Item2d item) { return this.item = item; }
-        public Item2d getItem() { return item; }
+        Menu menu = new Menu(view);
+        menu.menu();
+    }
+}
 
-        public double setOffset(double offset) { return this.offset = offset; }
-        public double getOffset() { return offset; }
+/* ================= MENU ================= */
+class Menu {
+    private TableView view;
+    private CommandManager manager = CommandManager.getInstance();
+    public Menu(TableView view) {
+        this.view = view;
+    }
+    public void menu() {
+        Scanner sc = new Scanner(System.in);
+        String cmd;
+        do {
+            System.out.println("\nКоманди:");
+            System.out.println("g - generate, v - view, s - save, r - restore");
+            System.out.println("u - undo, m - macro, q - exit");
+            cmd = sc.nextLine();
 
-        @Override
-        public void execute() {
-            if(item!=null){
-                prevY = item.getY();
-                item.setY(item.getY() * offset);
+            switch (cmd) {
+                case "g":
+                    manager.execute(new GenerateCommand(view));
+                    break;
+                case "v":
+                    manager.execute(new ShowCommand(view));
+                    break;
+                case "s":
+                    manager.execute(new SaveCommand(view));
+                    break;
+                case "r":
+                    manager.execute(new RestoreCommand(view));
+                    break;
+                case "u":
+                    manager.undo();
+                    break;
+                case "m":
+                    MacroCommand macro = new MacroCommand();
+                    macro.add(new GenerateCommand(view));
+                    macro.add(new ShowCommand(view));
+                    manager.execute(macro);
+                    break;
+                case "q":
+                    System.out.println("Вихід.");
+                    break;
+                default:
+                    System.out.println("Невірна команда.");
             }
-        }
 
-        public void undo() {
-            if(item!=null) item.setY(prevY);
+        } while (!cmd.equals("q"));
+    }
+}
+
+/* ================= COMMAND ================= */
+
+interface Command {
+    void execute();
+
+    void undo();
+}
+
+/* ================= SINGLETON ================= */
+
+class CommandManager {
+
+    private static CommandManager instance;
+    private Stack<Command> history = new Stack<>();
+
+    private CommandManager() {
+    }
+
+    public static CommandManager getInstance() {
+        if (instance == null)
+            instance = new CommandManager();
+        return instance;
+    }
+
+    public void execute(Command cmd) {
+        cmd.execute();
+        history.push(cmd);
+    }
+
+    public void undo() {
+        if (!history.isEmpty()) {
+            history.pop().undo();
+        } else {
+            System.out.println("Немає що скасувати.");
+        }
+    }
+}
+
+/* ================= COMMANDS ================= */
+
+class GenerateCommand implements Command {
+
+    private TableView view;
+
+    public GenerateCommand(TableView v) {
+        this.view = v;
+    }
+
+    public void execute() {
+        view.init();
+        view.show();
+    }
+
+    public void undo() {
+        view.clear();
+        System.out.println("Undo генерації.");
+    }
+}
+
+class ShowCommand implements Command {
+
+    private TableView view;
+
+    public ShowCommand(TableView v) {
+        this.view = v;
+    }
+
+    public void execute() {
+        view.show();
+    }
+
+    public void undo() {
+        System.out.println("Undo перегляду неможливий.");
+    }
+}
+
+class SaveCommand implements Command {
+
+    private TableView view;
+
+    public SaveCommand(TableView v) {
+        this.view = v;
+    }
+
+    public void execute() {
+        try {
+            view.save();
+            System.out.println("Збережено.");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    // ===================== View =====================
-    public interface View {
-        void viewInit();
-        void viewShow();
-        void viewSave() throws IOException;
-        void viewRestore() throws Exception;
-        ArrayList<Item2d> getItems();
+    public void undo() {
+        System.out.println("Undo збереження неможливий.");
+    }
+}
+
+class RestoreCommand implements Command {
+
+    private TableView view;
+
+    public RestoreCommand(TableView v) {
+        this.view = v;
     }
 
-    // ===================== ViewResult =====================
-    public static class ViewResult implements View {
-        private static final String FNAME="items.ser";
-        protected ArrayList<Item2d> items = new ArrayList<>();
-
-        public ViewResult() { this(10); }
-        public ViewResult(int n) { for(int i=0;i<n;i++) items.add(new Item2d()); }
-
-        @Override public ArrayList<Item2d> getItems() { return items; }
-
-        protected double calc(double x){ return Math.sin(Math.toRadians(x)); }
-
-        @Override
-        public void viewInit() {
-            double step = Math.random()*10+1;
-            double x=0;
-            for(Item2d item: items){ item.setXY(x, calc(x)); x+=step; }
-        }
-
-        @Override
-        public void viewShow() {
-            System.out.println("Колекція Item2d:");
-            for(Item2d i: items) System.out.println(i);
-            System.out.println("Кінець колекції.\n");
-        }
-
-        @Override
-        public void viewSave() throws IOException {
-            try(ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(FNAME))){
-                os.writeObject(items);
-            }
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public void viewRestore() throws Exception {
-            try(ObjectInputStream is = new ObjectInputStream(new FileInputStream(FNAME))){
-                items = (ArrayList<Item2d>)is.readObject();
-            }
+    public void execute() {
+        try {
+            view.restore();
+            view.show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    // ===================== ViewConsoleCommand =====================
-    public static class ViewConsoleCommand implements ConsoleCommand {
-        private View view;
-        public ViewConsoleCommand(View view) { this.view=view; }
-        @Override public char getKey(){ return 'v'; }
-        @Override public void execute(){ System.out.println("View collection"); view.viewShow(); }
-        @Override public String toString(){ return "'v'iew"; }
+    public void undo() {
+        System.out.println("Undo відновлення неможливий.");
+    }
+}
+
+/* ================= MACRO ================= */
+
+class MacroCommand implements Command {
+
+    private List<Command> list = new ArrayList<>();
+
+    public void add(Command c) {
+        list.add(c);
     }
 
-    // ===================== GenerateConsoleCommand =====================
-    public static class GenerateConsoleCommand implements ConsoleCommand {
-        private View view;
-        public GenerateConsoleCommand(View view) { this.view=view; }
-        @Override public char getKey(){ return 'g'; }
-        @Override public void execute(){ System.out.println("Generate collection"); view.viewInit(); view.viewShow(); }
-        @Override public String toString(){ return "'g'enerate"; }
+    public void execute() {
+        for (Command c : list)
+            c.execute();
     }
 
-    // ===================== ChangeConsoleCommand =====================
-    public static class ChangeConsoleCommand extends ChangeItemCommand implements ConsoleCommand {
-        private View view;
-        public ChangeConsoleCommand(View view){ this.view=view; }
-        @Override public char getKey(){ return 'c'; }
-        @Override public String toString(){ return "'c'hange"; }
-        @Override public void execute(){
-            System.out.println("Change items");
-            setOffset(Math.random()*100.0);
-            for(Item2d item: view.getItems()){ setItem(item); super.execute(); }
-            view.viewShow();
-        }
+    public void undo() {
+        for (int i = list.size() - 1; i >= 0; i--)
+            list.get(i).undo();
+    }
+}
+
+/* ================= VIEW ================= */
+
+interface View {
+    void show();
+}
+
+/* ================= FACTORY ================= */
+
+interface Viewable {
+    View getView();
+}
+
+class ViewableResultExtended implements Viewable {
+    public View getView() {
+        return new TableView();
+    }
+}
+
+/* ================= DATA ================= */
+
+class NumberData implements Serializable {
+    int number;
+    String oct;
+    String hex;
+
+    public NumberData(int n) {
+        number = n;
+    }
+}
+
+/* ================= TABLE VIEW ================= */
+
+class TableView implements View {
+
+    private static final String FILE = "data.ser";
+
+    private ArrayList<NumberData> list = new ArrayList<>();
+    private int width = 50;
+    private int count = 5;
+
+    public void setWidth(int w) {
+        width = w;
     }
 
-    // ===================== SaveConsoleCommand =====================
-    public static class SaveConsoleCommand implements ConsoleCommand {
-        private View view;
-        public SaveConsoleCommand(View view){ this.view=view; }
-        @Override public char getKey(){ return 's'; }
-        @Override public String toString(){ return "'s'ave"; }
-        @Override public void execute(){
-            System.out.println("Save collection");
-            try{ view.viewSave(); } catch(IOException e){ System.err.println(e); }
-        }
+    public void setCount(int c) {
+        count = c;
     }
 
-    // ===================== RestoreConsoleCommand =====================
-    public static class RestoreConsoleCommand implements ConsoleCommand {
-        private View view;
-        public RestoreConsoleCommand(View view){ this.view=view; }
-        @Override public char getKey(){ return 'r'; }
-        @Override public String toString(){ return "'r'estore"; }
-        @Override public void execute(){
-            System.out.println("Restore collection");
-            try{ view.viewRestore(); } catch(Exception e){ System.err.println(e); }
-            view.viewShow();
-        }
-    }
-
-    // ===================== Menu (макрокоманда) =====================
-    public static class Menu implements Command {
-        private List<ConsoleCommand> menu = new ArrayList<>();
-        private Stack<ChangeItemCommand> undoStack = new Stack<>();
-
-        public ConsoleCommand add(ConsoleCommand c){ menu.add(c); return c; }
-
-        @Override
-        public void execute(){
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String s;
-            menu: while(true){
-                do{
-                    System.out.print(this);
-                    try{ s=in.readLine(); } catch(IOException e){ System.err.println(e); return; }
-                }while(s.length()!=1);
-                char key = s.charAt(0);
-                if(key=='q'){ System.out.println("Exit"); break menu; }
-
-                boolean found=false;
-                for(ConsoleCommand c: menu){
-                    if(c.getKey()==key){
-                        if(c instanceof ChangeConsoleCommand)
-                            undoStack.push((ChangeConsoleCommand)c);
-                        c.execute();
-                        found=true; break;
-                    }
-                }
-                if(!found) System.out.println("Wrong command.");
-            }
-        }
-
-        @Override
-        public String toString(){
-            StringBuilder sb=new StringBuilder("Enter command:\n");
-            for(ConsoleCommand c: menu) sb.append(c+", ");
-            sb.append("'q'uit: ");
-            return sb.toString();
-        }
-
-        public void undo(){
-            if(!undoStack.isEmpty()){
-                ChangeItemCommand cmd = undoStack.pop();
-                cmd.undo();
-            }
+    public void init() {
+        list.clear();
+        for (int i = 0; i < count; i++) {
+            int n = (int) (Math.random() * 1000);
+            NumberData d = new NumberData(n);
+            d.oct = Integer.toOctalString(n);
+            d.hex = Integer.toHexString(n);
+            list.add(d);
         }
     }
 
-    // ===================== Application (Singleton) =====================
-    public static class Application {
-        private static Application instance = new Application();
-        private View view = new ViewResult();
-        private Menu menu = new Menu();
-        private Application(){ }
-        public static Application getInstance(){ return instance; }
-        public void run(){
-            menu.add(new ViewConsoleCommand(view));
-            menu.add(new GenerateConsoleCommand(view));
-            menu.add(new ChangeConsoleCommand(view));
-            menu.add(new SaveConsoleCommand(view));
-            menu.add(new RestoreConsoleCommand(view));
-            menu.execute();
+    public void show() {
+        System.out.println("\nТаблиця:");
+        System.out.println("-".repeat(width));
+        for (NumberData d : list) {
+            System.out.println(d.number + " | " + d.oct + " | " + d.hex);
         }
+        System.out.println("-".repeat(width));
     }
 
-    // ===================== Main =====================
-    public static class Main {
-        public static void main(String[] args){
-            Application app = Application.getInstance();
-            app.run();
-        }
+    public void clear() {
+        list.clear();
+    }
+
+    public void save() throws Exception {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE));
+        oos.writeObject(list);
+        oos.close();
+    }
+
+    public void restore() throws Exception {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE));
+        list = (ArrayList<NumberData>) ois.readObject();
+        ois.close();
+    }
+}
+
+/* ================= TEST ================= */
+
+class Task5Test {
+    public static void main(String[] args) {
+
+        TableView view = new TableView();
+        view.setCount(3);
+
+        CommandManager m = CommandManager.getInstance();
+
+        m.execute(new GenerateCommand(view));
+        m.execute(new ShowCommand(view));
+
+        System.out.println("Undo:");
+        m.undo();
     }
 }
